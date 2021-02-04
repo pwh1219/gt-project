c367ce6d6f5287785a7a5239cb96895b5d5275e8
pwh1219 <pwh1219@163.com>
2021-02-04 14:59:58 +0800

    列车长后台门户

---
+++ gt-parent/gt-conductor/src/main/java/com/gt/GtConductorApplication.java
@@ -0,0 +1,13 @@
+package com.gt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtConductorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GtConductorApplication.class, args);
+    }
+
+}
