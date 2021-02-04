81be671f90b1e3aa14b5bb08d083dfb03a53d5d6
pwh1219 <pwh1219@163.com>
2021-02-04 15:01:09 +0800

    外部用户列车商城

---
+++ gt-parent/gt-supermarket/src/main/java/com/gt/GtSupermarketApplication.java
@@ -0,0 +1,13 @@
+package com.gt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtSupermarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GtSupermarketApplication.class, args);
+    }
+
+}
