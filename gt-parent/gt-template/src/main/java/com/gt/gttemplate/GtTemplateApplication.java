7343a5d98dc73ccf1b09beeba65a590b83a51fda
z <812424695@qq.com>
2021-02-08 13:13:33 +0800

    创建模板项目

---
+++ gt-parent/gt-template/src/main/java/com/gt/gttemplate/GtTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.gt.gttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GtTemplateApplication.class, args);
+    }
+
+}
