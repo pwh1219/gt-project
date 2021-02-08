43a9ca854e54f35e6ac9f08e757b205bba65b657
z <812424695@qq.com>
2021-02-08 10:44:18 +0800

    删除

---
+++ gt-parent/gt-manage/src/main/java/com/gt/gtmanage/GtManageApplication.java
@@ -0,0 +1,13 @@
+package com.gt.gtmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GtManageApplication.class, args);
+    }
+
+}
