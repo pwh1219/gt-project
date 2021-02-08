acbfe37d7d8a6c66b09784bba43b848e4337d4a1
z <812424695@qq.com>
2021-02-08 14:15:55 +0800

    创建公共项目

---
+++ gt-parent/gt-common/src/main/java/com/gt/gtcommon/GtCommonApplication.java
@@ -0,0 +1,13 @@
+package com.gt.gtcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GtCommonApplication.class, args);
+    }
+
+}
