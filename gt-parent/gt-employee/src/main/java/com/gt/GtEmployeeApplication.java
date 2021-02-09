7ed6e19f298b571c5147b93dac4f40d4ed0e7732
pwh1219 <pwh1219@163.com>
2021-02-09 16:30:03 +0800

    新增员工管理系统后台

---
+++ gt-parent/gt-employee/src/main/java/com/gt/GtEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.gt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtEmployeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GtEmployeeApplication.class, args);
+    }
+
+}
