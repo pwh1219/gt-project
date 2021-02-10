d0ab84f8abb232003e37ec43e6d2ddb173b932c7
pwh1219 <pwh1219@163.com>
2021-02-10 15:54:57 +0800

    新增乘务后台管理

---
+++ gt-parent/gt-crew/src/main/java/com/gt/GtCrewApplication.java
@@ -0,0 +1,13 @@
+package com.gt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtCrewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GtCrewApplication.class, args);
+    }
+
+}
